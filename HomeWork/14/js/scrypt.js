f12efe196a9ce82d0e9cb695ccf24fb177c56fd4
Serhii Koziar <lytuishmel@gmail.com>
2021-12-15 23:23:19 +0200

    HW 14

---
+++ HomeWork/14/js/scrypt.js
@@ -0,0 +1,7 @@
+function closeModal() {
+    document.getElementById("modalx").style.display = "none";
+}
+
+function openModal() {
+    document.getElementById("modalx").style.display = "block";
+}
